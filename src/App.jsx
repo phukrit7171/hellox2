1d258b4a587237a3886c9d8dbabc113a1e655dbd
Phukrit Kittinontana <phukrit7171@gmail.com>
2025-10-16 15:33:40 +0700

    fix: add line breaks to improve spacing in App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <h1>Hello World</h1>
+      <h1>Hello World</h1><br /><br />
       <h2>Deploy to GitHub Pages</h2>
     </div>
   )
