07f7eb022fbbc85a780abdba55b7e5f93b47ce04
Phukrit Kittinontana <phukrit7171@gmail.com>
2025-10-16 14:55:17 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <h1>Hello World</h1>
+      <h2>Deploy to GitHub Pages</h2>
+    </div>
+  )
+}
+
+export default App
